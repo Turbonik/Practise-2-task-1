55c7947f42c74d295a2325387da5bdff4e9e39b4
2013o <2013o@DESKTOP-KGS0845>
2024-10-13 20:34:19 +0700

    Добавьте файлы проекта.

---
+++ pr2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+      
+                Console.Write("Введите длину массива:");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int[] nums = new int[n];
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    nums[i] = i + 1;
+                }
+
+                Array.Reverse(nums);
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(nums[i]);
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
